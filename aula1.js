aa588eddd4fc8042be7a440aea1b67f2860dfb2c
MarcosVCarvalho <carvalho.marcosvs@gmail.com>
2025-08-27 10:49:15 -0300

    Initial commit

---
+++ aula1.js
@@ -0,0 +1,40 @@
+//Questão 1
+let name = 'Marcos';
+let numero = 20;
+let boleano = false;
+console.log(name,numero,boleano);
+
+//Questão 2
+let primeiro_nome =  'Vinicius ';
+let ultimo_nome =  'Silva';
+let nome_completo = 'Meu nome é ${primeiro_nome} ${ultimo_nome}';
+console.log(nome_completo);
+
+//Questão 3
+let idade = 20;
+let text_idade = 'Minha idade é ';
+let minha_idade = idade + text_idade;
+console.log(minha_idade);
+
+//Questão 4
+let v1 = 1;
+console.log(v1);
+v1 = 2;
+console.log(v1);
+
+//Questão 5
+if variavel2 == 2:
+    variavel2 = 3;
+    console.log(variavel2);
+
+var variavel2 = 2;
+
+console.log(variavel2);
+
+//Questão 6
+let chuva = true;
+if chuva == true:
+    console.log('Está chovendo - Leve o guarda chuva')
+else:
+    console.log('Não está chovendo - Não leve o guarda chuva')
+
